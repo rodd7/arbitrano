478da6bd67261daad0fac7a2d1261ca3edc4fbd0
rodd7 <69749721+rodd7@users.noreply.github.com>
2022-11-15 19:12:59 +1100

    reading from json

--- tennis.py
+++ tennis.py
@@ -4,18 +4,20 @@ from selenium.webdriver.chrome.service import Service
 
 import time
 from collections import defaultdict
+import json
 
-# CHROME_PATH = "C:\Program Files\chromedriver_win32\chromedriver.exe"
-CHROME_PATH = "/usr/local/bin/chromedriver" #mac version
+CHROME_PATH = "C:\Program Files\chromedriver_win32\chromedriver.exe"
+# CHROME_PATH = "/usr/local/bin/chromedriver" #mac version
 
+URLSize = 0
+URLCount = -1
 
 def main():
     data = defaultdict(list)
     initDriver = Service(CHROME_PATH)
     
 
-    global driver 
-    urlCount = 0
+    global driver
 
     driver = webdriver.Chrome(service = initDriver)
     options = webdriver.ChromeOptions()
@@ -23,70 +25,49 @@ def main():
     options.add_argument('--ignore-ssl-errors')
     options.add_argument('--ignore-certificate-errors-spki-list')
 
-    sportsbet(driver, data)
-    # pointsbet(driver, data)
-    ladbrokes(driver, data, urlCount)
+    file = open('arbitrano.json')
+    arbitrano = json.load(file)
+    setURLSize(len(arbitrano['betters']))
+    for better in arbitrano['betters']:
+        initialScrape(better['URL'], better['playerSelector'], better['oddSelector'], data)
+
+
+    
+
     print(data)
-    # sortedDict = dict(sorted(data.items(), key=lambda x: x[0].lower()) ) #this will sort our players
     
     # for k, v in data.items():
     #     print(k, v)
 
     driver.quit()
 
+def setURLSize(x):
+    global URLSize
+    URLSize = x
+
+def setURLCount():
+    global URLCount
+    URLCount+=1
 
 def initialScrape(URL, playerSelector, oddSelector, data):
     driver.get(URL)
     time.sleep(3)
-    initStore(URL, driver.find_elements(By.CSS_SELECTOR,playerSelector), driver.find_elements(By.CSS_SELECTOR,oddSelector), data)
+    setURLCount()
+    initStore(driver.find_elements(By.CSS_SELECTOR,playerSelector), driver.find_elements(By.CSS_SELECTOR,oddSelector), data)
 
-
-
-def sportsbet(driver, data):
-    driver.get('https://www.sportsbet.com.au/betting/tennis')
-    time.sleep(3)
-    #all bt click in and out for each one
-    # driver.execute_script("window.history.go(-1)")
-   
-    # driver.find_element(By.XPATH, "/html/body/span/div/div/div[2]/div/div[3]/div/div/div[1]/div/div/div/div/div[1]/div[2]/div[2]/ul/li[1]/div/div/a/div/div[2]/span").click()
-    
-
-    players = driver.find_elements(By.CSS_SELECTOR,"span[data-automation-id*='outcome-name']")
-    numbers = driver.find_elements(By.CSS_SELECTOR,"span[data-automation-id*='price-text']")
-    initStore(players, numbers, data)
-
-
-def pointsbet(driver, data):
-    driver.get('https://pointsbet.com.au/sports/tennis')
-    time.sleep(3)
-    players = driver.find_elements(By.CSS_SELECTOR,"span[class='f5rl2hl']")
-    numbers = driver.find_elements(By.CSS_SELECTOR,"span[class='fheif50']")
-    initStore(players, numbers, data)
-
-def ladbrokes(driver, data, urlCount):
-    urlCount = 2
-    driver.get('https://www.ladbrokes.com.au/sports/tennis')
-    time.sleep(3)
-    players = driver.find_elements(By.CSS_SELECTOR,"span[class='displayTitle']")
-    numbers = driver.find_elements(By.CSS_SELECTOR,"div[class='price-button-odds-price']")
-    initStore(players, numbers, data)
 
 def initStore(players, numbers, data):
+    print(URLSize)
     for x in range(len(players)):
-        data[players[x].text].append(numbers[x].text)
+        data[players[x].text] = ['0.00'] * URLSize
+        for y in range(len(data[players[x].text])):
+            if y == URLCount:
+                data[players[x].text][y] = numbers[x].text
 
-def store(players, numbers, data):
-    for player in range(len(data)):
-        if players[player].text in data:
-            data[players[player].text].append(numbers[player].text)
-        else:
-            data[players[player].text].append('0.00')
-
-
-
-
-#if the keys exists, append to the array of values
-#else create a new one and add zeroes until the required value in position
+# def initStore(players, numbers, data):
+#     print(URLSize)
+#     for x in range(len(players)):
+#         data[players[x].text].append(numbers[x].text)
 
 
 # https://marktheballot.blogspot.com/2018/06/the-dramas-of-daily-web-scraper.html
