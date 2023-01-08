aebeec17a12395abf42cc55073c060bccf010fdc
rodd7 <69749721+rodd7@users.noreply.github.com>
2023-01-08 23:35:46 +1100

    re-init

--- tennis2.py
+++ tennis2.py
@@ -11,17 +11,49 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.common.exceptions import TimeoutException
 
 import time
-from collections import defaultdict
-import collections
-import json
+from collections import namedtuple
 from arbitrage import *
 
-
+#########################  WEBDRIVER SOURCE  #########################
 CHROME_PATH = "C:\Program Files\chromedriver_win32\chromedriver.exe"
 # CHROME_PATH = "/usr/local/bin/chromedriver" #mac version
+######################################################################
 
 def main():
-    print("test")
+
+    initDriver = Service(CHROME_PATH)
+    global driver
+
+    data = {}
+
+    driver = webdriver.Chrome(service = initDriver)
+    options = webdriver.ChromeOptions()
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--ignore-ssl-errors')
+    options.add_argument('--ignore-certificate-errors-spki-list')
+
+    driver.get('https://pointsbet.com.au/sports/tennis')
+    players = driver.find_elements(By.CSS_SELECTOR,"span[class='f5rl2hl']")
+    numbers = driver.find_elements(By.CSS_SELECTOR,"span[class='fheif50']")
+
+    for x in range(len(players)):
+        print()
+    #time.sleep(3)
+
+    driver.quit()
+
+    
+    # wrong, need to research
+
+    # match = namedtuple('match', ['name1', 'name2'])
+
+    # data = match([], [])
+    # data.name1.append((1, 2))
+    # data.name1.extend([(3, 4), (5, 6)])
+
+    # print(data)  # prints MyDataType(name1=[(1, 2), (3, 4), (5, 6)], name2=[])
+
+
 
 if __name__ == "__main__":
     main()
