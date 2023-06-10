cecdb4c03addd27aba2fd0d9d612ad4f68473e6d
rodd7 <69749721+rodd7@users.noreply.github.com>
2023-06-10 16:23:21 +1000

    new approach
    still needs to loop through and scroll all elements

--- tennis2.py
+++ tennis2.py
@@ -1,48 +1,94 @@
-#this version is going to be improved
-#everything has to be re-approached because some websites have different configurations and making them similar will break and be harder
-#don't use defaultdict, be creative
-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.chrome.options import Options
 
-from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from selenium.common.exceptions import ElementClickInterceptedException
 
 import time
-from collections import namedtuple
-from arbitrage import *
 
 #########################  WEBDRIVER SOURCE  #########################
 CHROME_PATH = "C:\Program Files\chromedriver_win32\chromedriver.exe"
 # CHROME_PATH = "/usr/local/bin/chromedriver" #mac version
 ######################################################################
 
-def main():
 
+def main():
     initDriver = Service(CHROME_PATH)
     global driver
 
     data = {}
+    webindex = 0
 
-    driver = webdriver.Chrome(service = initDriver)
+    driver = webdriver.Chrome(service=initDriver)
+    actions = ActionChains(driver)
+
     options = webdriver.ChromeOptions()
-    options.add_argument('--ignore-certificate-errors')
-    options.add_argument('--ignore-ssl-errors')
-    options.add_argument('--ignore-certificate-errors-spki-list')
+    options.add_argument("--ignore-certificate-errors")
+    options.add_argument("--ignore-ssl-errors")
+    options.add_argument("--ignore-certificate-errors-spki-list")
 
-    driver.get('https://pointsbet.com.au/sports/tennis')
-    players = driver.find_elements(By.CSS_SELECTOR,"span[class='f5rl2hl']")
-    numbers = driver.find_elements(By.CSS_SELECTOR,"span[class='fheif50']")
+    driver.get("https://pointsbet.com.au/sports/tennis")
+    time.sleep(5)
 
-    for x in range(len(players)):
-        print()
-    #time.sleep(3)
+    driver.find_element(
+        By.CSS_SELECTOR, "button[data-test='sportsSportsMain2TabButton']"
+    ).click()
+
+    tournamentLists = driver.find_elements(
+        By.CSS_SELECTOR, "a.f147sodr.f73eam3.f17kgnnr.f4ru8xs.f1rtv67w"
+    )
+
+    scrollLevel = 0
+    wait = WebDriverWait(driver, 10)
+
+    for x in range(len(tournamentLists)):
+        try:
+            if x > 0:
+                driver.execute_script(f"window.scrollTo({0}, {0})")
+                wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "body")))
+                time.sleep(1)
+                driver.find_element(
+                    By.CSS_SELECTOR, "button[data-test='sportsSportsMain2TabButton']"
+                ).click()
+
+            # driver.execute_script(f"window.scrollTo({0}, {scrollLevel})")
+            driver.execute_script(f"window.scrollTo({0}, {scrollLevel})")
+            tournamentLists = driver.find_elements(
+                By.CSS_SELECTOR, "a.f147sodr.f73eam3.f17kgnnr.f4ru8xs.f1rtv67w"
+            )
+            time.sleep(2)
+            actions.move_to_element(tournamentLists[x]).perform()
+            # wait.until(
+            #     EC.element_to_be_clickable(
+            #         (
+            #             By.CSS_SELECTOR,
+            #             "a.f147sodr.f73eam3.f17kgnnr.f4ru8xs.f1rtv67w:nth-of-type(%d)"
+            #             % (x + 1),
+            #         )
+            #     )
+            # )
+
+            print(tournamentLists[x].location)
+
+            scrollLevel += 50
+
+            time.sleep(1)
+            tournamentLists[x].click()
+            time.sleep(1)
+            driver.back()
+
+            time.sleep(1)
+        except Exception as e:
+            print(f"the value of {x} doesn't pint")
 
     driver.quit()
 
-    
     # wrong, need to research
 
     # match = namedtuple('match', ['name1', 'name2'])
@@ -52,7 +98,6 @@ def main():
     # data.name1.extend([(3, 4), (5, 6)])
 
     # print(data)  # prints MyDataType(name1=[(1, 2), (3, 4), (5, 6)], name2=[])
-
 
 
 if __name__ == "__main__":
